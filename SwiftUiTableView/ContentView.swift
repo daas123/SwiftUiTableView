9a40a5a442fbe1f8bc8e6f81f1c8fcf719c69b33
saad <saadneosoft@gmail.com>
2023-10-23 11:52:02 +0530

    basic TableView Done

--- SwiftUiTableView/ContentView.swift
+++ SwiftUiTableView/ContentView.swift
@@ -8,18 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    var values = [
+        Formate(name: "saad", details: "hello there"),
+        Formate(name: "daad", details: "hello world")
+    ]
+    
     var body: some View {
-        ZStack{
-            VStack{
-                Text("This is default TableView")
-                    .bold()
-                    .font(.title2)
-                List (1..<12) {
-                    Text("\($0)")
+        VStack{
+            Text("This is default TableView")
+                .bold()
+                .font(.title2)
+            
+            List{
+                Section(header: Text("Section1"),footer:Text("Footer")){
+                    ForEach(1..<6){
+                        Text("\($0)")
+                    }
+                }.listStyle(InsetGroupedListStyle())
+                .listRowInsets(EdgeInsets.init(top:30,leading: 20, bottom: 30, trailing: 0))
+                
+                Section(header: Text("Header 2"),footer:Text("Footer 2")){
+                    ForEach(6..<11){
+                        Text("\($0)")
+                    }
                 }
+            }.listStyle(GroupedListStyle())
+            
+            
+            List {
+                Section {
+                    ForEach(values) { value in
+                        Text(value.name)
+                    }
+                } header: {
+                    Text("this Custom Header")
+                }footer: {
+                    Text("This is custom Footer")
+                }
+            }.listStyle(SidebarListStyle())
+            
+            List(values) { value in
+                Text(value.name)
             }
-        }
-        .padding()
+                
+            
+            
+        }.padding()
+        
     }
 }
 
@@ -27,4 +62,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct Formate:Identifiable{
+    var id = UUID()
+    let name : String
+    let details : String
 }
