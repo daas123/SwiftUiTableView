b61d37651c629897be0ad42c41cf6454bf848f84
saad <saadneosoft@gmail.com>
2023-10-23 16:34:54 +0530

    done Delete Function is Working

--- SwiftUiTableView/ContentView.swift
+++ SwiftUiTableView/ContentView.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newdata = [1,2,3,4,5,6,7,8]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            List{
+                Section {
+                    ForEach(newdata , id: \.self){
+                        num in
+                        Text("\(num)")
+                    }
+                    .onDelete(perform: deleteRow)
+                    
+                }header: {
+                    Text("Basic Editable View")
+                }
+            }.navigationBarItems(leading: EditButton(), trailing: addButton)
         }
-        .padding()
     }
+    var addButton: some View {
+            Button(action: {
+                newdata.append(newdata.count + 1)
+            }) {
+                Image(systemName: "plus")
+            }
+        }
+    func deleteRow(at offsets: IndexSet) {
+        newdata.remove(atOffsets: offsets)
+       }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
