dc45254fc707071db3530bf3500847968f5cd2d7
saad <saadneosoft@gmail.com>
2023-10-23 10:47:37 +0530

    Initial Commit

---
+++ SwiftUiTableView/SwiftUiTableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTableViewApp.swift
+//  SwiftUiTableView
+//
+//  Created by Neosoft on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
